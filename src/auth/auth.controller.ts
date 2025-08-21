7bcfcaffccdc8215705698a68634af65bf8968cc
eman <emanadelibrahim2@gmail.com>
2025-08-21 16:29:12 +0300

    connect keycloack to manage roles

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,9 +2,9 @@ import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dtos/register.dto/register.dto';
 import { LoginDto } from './dtos/login.dto/login.dto';
-import { LocalAuthGuard } from './guards/local-auth.guard';
-import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
-
+// import { LocalAuthGuard } from './guards/local-auth.guard';
+// import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
+import { KeycloakAuthGuard } from 'nest-keycloak-connect';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly auth: AuthService) {}
@@ -15,25 +15,25 @@ export class AuthController {
   }
 
   // Passport Local guard puts validated user on req.user
-  @UseGuards(LocalAuthGuard)
+  @UseGuards(KeycloakAuthGuard)
   @Post('login')
   async login(@Req() req: any) {
     return this.auth.login(req.user);
   }
 
-  @Post('refresh')
-  async refresh(@Body() body: {  refreshToken: string }) {
-    return this.auth.refresh( body.refreshToken);
-  }
+  // @Post('refresh')
+  // async refresh(@Body() body: {  refreshToken: string }) {
+  //   return this.auth.refresh( body.refreshToken);
+  // }
 
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(KeycloakAuthGuard)
   @Post('logout')
   async logout(@Req() req: any, @Body() body: { refreshToken: string }) {
     return this.auth.logout(req.user.sub, body.refreshToken);
   }
 
   // Quick protected ping for testing guards
-  @UseGuards(JwtAuthGuard)
+  @UseGuards(KeycloakAuthGuard)
   @Get('protected-ping')
   ping(@Req() req: any) {
     return { ok: true, user: req.user };
